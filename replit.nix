2dcc95ec8adeaa8e5e99baf40e04c21ce3997f54
boweazy <45015547+boweazy@users.noreply.github.com>
2026-02-20 03:21:20 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,9 +1,12 @@
 { pkgs }: {
   deps = [
-    pkgs.nano
-    pkgs.wget
-    pkgs.imagemagick
-    pkgs.dejavu_fonts   # DejaVu-Sans
-    pkgs.noto-fonts     # wide glyph coverage
+    pkgs.python311Full
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.pydantic
+    pkgs.python311Packages.httpx
+    pkgs.python311Packages.python-dotenv
+    pkgs.python311Packages.pillow
   ];
 }
